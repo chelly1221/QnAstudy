fffc6142c01b0481cb0134daa1506dbe62680562
chelly1221 <chelly1221.com@gmail.com>
2019-04-28 20:30:25 +0900

    test

--- Java/helloworld.java
+++ Java/helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld {
 
     public static void main(String[] args) {
-        System.out.println(" §±§§§∑§§§∑§© ");
+        System.out.println(" „ÖÅ„Ñ¥„Öá„Ñ¥„Öá„Ñπ ");
     }
 }
 
