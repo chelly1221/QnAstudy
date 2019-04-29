72972a534440c4c11e012f2a20e5635f086704e3
chelly1221 <chelly1221.com@gmail.com>
2019-04-29 13:58:08 +0900

    커밋

--- Java/helloworld.java
+++ Java/helloworld.java
@@ -1,7 +1,7 @@
-public class helloworld {
+﻿public class helloworld {
 
     public static void main(String[] args) {
-        System.out.println("�ȳ�21");
+        System.out.println("안녕21");
     }
 }
 
