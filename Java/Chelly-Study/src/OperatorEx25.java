bd151541c47191432ea463b20821e1f0e94924a2
chelly1221 <chelly1221.com@gmail.com>
2019-05-14 17:32:40 +0900

    2019-05-14

---
+++ Java/Chelly-Study/src/OperatorEx25.java
@@ -0,0 +1,20 @@
+import java.util.*;   // 아직도 scanner 쓸떄 이거 추가하는 이유를 잘 모르게따
+
+public class OperatorEx25 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char ch = ' ';
+		
+		System.out.printf("문자를 하나 입력하세요.>");
+		
+		String input = scanner.nextLine();
+		ch = input.charAt(0);
+		
+		if('0'<=ch&&ch<='9') {
+			System.out.printf("입력하신 문자는 숫자입니다.%n");
+		}
+		if(('a'<=ch&&ch<='z') || ('A'<= ch && ch <= 'Z')) {
+			System.out.printf("입력하신 문자는 영문자입니다.%n");
+		}
+	}
+}
