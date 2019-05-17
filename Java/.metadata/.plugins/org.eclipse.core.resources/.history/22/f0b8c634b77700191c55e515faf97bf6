2f0b430a33450a4037eb9ffc2075a4df7f156375
chelly1221 <chelly1221.com@gmail.com>
2019-05-17 15:05:49 +0900

    2019-05-17 Chapter4 Complete!

---
+++ Java/.metadata/.plugins/org.eclipse.core.resources/.history/22/f0b8c634b77700191c55e515faf97bf6
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class FlowEx33 {
+	public static void main(String[] args) {
+		int menu = 0;
+		int num = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println("(1) square");
+			System.out.println("(2) square root");
+			System.out.println("(3) log");
+			System.out.print("1~3중에 원하는 메뉴 선택 (종료:0)>");
+			
+			String tmp = scanner.nextLine();
+			menu = Integer.parseInt(tmp);
+			
+			if(menu==0) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else if (!(1 <= menu && menu <= 3)) {
+				System.out.println("메뉴를 잘못 선택하셨습니다. (종료는 0)");
+				continue;
+			}
+			System.out.println("선택하신 메뉴는 "+ menu +"번입니다.");
+		}
+	}
+}
---
+++ Java/Chelly-Study/src/FlowEx34.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class FlowEx34 {
+	public static void main(String[] args) {
+		int menu =0 , num = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		outer:
+		while(true) {
+			System.out.println("(1) square");
+			System.out.println("(2) square root");
+			System.out.println("(3) log");
+			System.out.print("원하는 메뉴(1~3)를 선택하세요. (종료:0)>");
+			
+			String tmp = scanner.nextLine();
+			menu = Integer.parseInt(tmp);
+			
+			if(menu==0) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else if (!(1<= menu && menu <= 3)) {
+				System.out.println("메뉴를 잘못 선택하셨습니다. (종료는 0)");
+				continue;
+			}
+			
+			for(;;) {
+				System.out.print("계산할 값을 입력하세요.(계산 종료:0, 전체 종료:99);>");
+				tmp = scanner.nextLine();
+				num = Integer.parseInt(tmp);
+				
+				if(num==0)
+					break;
+				
+				if(num==99)
+					break outer;
+				
+				switch(menu) {
+				case 1:
+					System.out.println("result="+ num*num);
+					break;
+				case 2:
+					System.out.println("result="+ Math.sqrt(num));
+				case 3:
+					System.out.println("result="+ Math.log(num));
+					break;
+				}
+			}
+		}
+	}
+}
