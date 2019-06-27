35d10b67952a2466736afd52759d2048e9c3d595
devJRL <dev2sponge@gmail.com>
2019-06-28 05:54:40 +0900

    [sponge-190628] rcc (ES7-React-js-snippets)

--- Node.js/sponge_react/src/App.js
+++ Node.js/sponge_react/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+ * ES7-React-js-snippets
+ * https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
+ * rcc 자동완성 : Class 기반 리액트 앱 뼈대 구성
+ */
+class App extends Component {
+  render() {
+    return <div>app</div>;
+  }
 }
 
 export default App;
