e4e887e60697dfa8e91af9c2dae7f3806bb3ffce
devJRL <dev2sponge@gmail.com>
2019-06-30 22:51:22 +0900

    [sponge-190631] state, props, setState({key:value})

--- Node.js/sponge_react/src/components/Content.js
+++ Node.js/sponge_react/src/components/Content.js
@@ -2,35 +2,35 @@
 import React, { Component } from "react";
 
 export default class Content extends Component {
-  // init state
-  constructor(props) {
-    super(props);
-    this.state = {
-      head: {
-        title: "Hello, React!",
-        message: "Practice about React"
-      },
-      index: [
-        { id: 0, keyword: "Index" },
-        { id: 1, keyword: "state" },
-        { id: 2, keyword: "props" }
-      ],
-      contents: [
-        { id: 1, comment: "'state'는 클래스에서 상태로 저장된 값입니다." },
-        { id: 2, comment: "'props'는 다른 클래스로 전달하는 값입니다." }
-      ]
-    };
-  }
   render() {
     return (
       <div>
-        <h1>{this.state.head.title}</h1>
-        <h4>{this.state.head.message}</h4>
-        <h2>{this.state.index[0].keyword}</h2>
-        <article>
-          <li>{this.state.index[1].keyword}</li>
-          <li>{this.state.index[2].keyword}</li>
-        </article>
+        <font size="2" color="gray">
+          아래 목록을 클릭하면 페이지의 state를 변경하는 함수를 호출합니다.
+          <br />
+          변경된 state 값에 따라서 아래 내용이 변경됩니다.
+        </font>
+        <ul>
+          {this.props.index.map((contact, index) => {
+            return (
+              <li key={index}>
+                <a
+                  href="/"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.props.changeIndex(index);
+                  }}
+                >
+                  {this.props.index[index].keyword}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <font size="4">
+          {this.props.contents[this.props.nowIndex].emoji}&nbsp;&nbsp;
+          {this.props.contents[this.props.nowIndex].comment}
+        </font>
       </div>
     );
   }
